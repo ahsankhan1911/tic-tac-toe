f0826642c31f31cc45cf5e19a93a984307a3b4d5
ahsankhan1911 <ahsankhan1911@gmail.com>
2018-09-16 22:34:50 +0500

    intial set up

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+   <img src="/images/ttt_board.png" alt="ttt-bg" width="100%"></img>
+      </div>
+    );
+  }
+}
+
+export default App;
